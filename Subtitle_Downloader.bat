b28ce51c61d5bc0b4efc72bec0f9cb41fda75d95
waynewei <me@wayneclub.com>
2021-12-18 03:20:06 +0800

    support windows

--- Subtitle_Downloader.bat
+++ Subtitle_Downloader.bat
@@ -1,8 +1,17 @@
 @@ECHO OFF
 
-set/p URL="下載網址："
+set/p url="url: "
 pip install -r requirement.txt
-python subtitle_downloader.py %URL%
+
+@REM 下載位置
+set output=""
+@REM Disney+
+set email=""
+set password=""
+@REM ["en", "zh-Hant", "zh-HK", "all"]
+set language="zh-Hant"
+
+python subtitle_downloader.py %url% -o %output% -email %email% -password %password% -lang %language%
 pause
 
 @@ECHO OFF
