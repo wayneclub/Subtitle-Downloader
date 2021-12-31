ad1d66ca1d98093e6023249f85bf689613d4e379
waynewei <me@wayneclub.com>
2021-12-31 11:08:34 +0800

    fix bug

---
+++ make.sh
@@ -0,0 +1,6 @@
+msgfmt -o locales/en/LC_MESSAGES/main.mo locales/en/LC_MESSAGES/main
+msgfmt -o locales/en/LC_MESSAGES/common.utils.mo locales/en/LC_MESSAGES/common.utils
+msgfmt -o locales/en/LC_MESSAGES/services.disneyplus.mo locales/en/LC_MESSAGES/services.disneyplus
+msgfmt -o locales/zh-Hant/LC_MESSAGES/main.mo locales/zh-Hant/LC_MESSAGES/main
+msgfmt -o locales/zh-Hant/LC_MESSAGES/common.utils.mo locales/zh-Hant/LC_MESSAGES/common.utils
+msgfmt -o locales/zh-Hant/LC_MESSAGES/services.disneyplus.mo locales/zh-Hant/LC_MESSAGES/services.disneyplus
