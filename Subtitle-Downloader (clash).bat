60a417e2ab283cec69565e6991c66c6740eec4d8
waynewei <me@wayneclub.com>
2023-09-04 15:25:09 +0800

    update bat

--- Subtitle-Downloader (clash).bat
+++ Subtitle-Downloader (clash).bat
@@ -1,20 +1,11 @@
 @@ECHO OFF
 :start
-set http_proxy=http://127.0.0.1:7890 & set https_proxy=http://127.0.0.1:7890
 cls
 set/p url="url: "
 
-@REM Output directory
-set output=""
-@REM Disney+、HBOGO Asia
-set email=""
-set password=""
-@REM ["en", "zh-Hant", "zh-HK", "zh-Hans", "all"]
-set slang="zh-Hant"
+set proxy="http://127.0.0.1:7890"
 
-set proxy=""
-
-python subtitle_downloader.py %url% -o %output% -email %email% -password %password% -slang %slang% -p %proxy%
+python subtitle_downloader.py %url% -p %proxy%
 pause
 
 goto start
--- Subtitle-Downloader.bat
+++ Subtitle-Downloader.bat
@@ -3,17 +3,7 @@
 cls
 set/p url="url: "
 
-@REM Output directory
-set output=""
-@REM Disney+、HBOGO Asia
-set email=""
-set password=""
-@REM ["en", "zh-Hant", "zh-HK", "zh-Hans", "all"]
-set slang="zh-Hant"
-
-set proxy=""
-
-python subtitle_downloader.py %url% -o %output% -email %email% -password %password% -slang %slang% -p %proxy%
+python subtitle_downloader.py %url%
 pause
 
 goto start
